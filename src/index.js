d5d340377f2440aca252135d6547211f228d3c81
Albert <albert.barsamov@gmail.com>
2022-03-10 16:53:14 +0100

    create Store + Provider

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App'
 import 'antd/dist/antd.css'
 
+import store from './redux/store'
+import { Provider } from 'react-redux'
+
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
     , document.getElementById('root')
 )
