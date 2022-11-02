360a6d8af366f4a3b38b994981b9ee3500881c60
Alik64 <albert.barsamov@gmail.com>
2022-11-02 09:35:48 +0100

    Profile link update

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-import { Routes, Route, Link } from 'react-router-dom'
-import { Layout, Typography, Space } from 'antd'
-import { Navbar, Homepage, Exchanges, Cryptos, CryptoDetails, News } from './components'
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  Cryptos,
+  CryptoDetails,
+  News,
+} from "./components";
 
 const App = () => {
-    return (
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
 
-        <div className='app'>
-
-            <div className="navbar">
-                <Navbar />
-            </div>
-
-
-            <div className="main">
-                <Layout>
-                    <div className="routes">
-                        <Routes>
-                            <Route path='/' element={<Homepage />} />
-                            <Route path='exchanges' element={<Exchanges />} />
-                            <Route path='cryptos' element={<Cryptos />} />
-                            <Route path='crypto/:coinId' element={<CryptoDetails />} />
-                            <Route path='news' element={<News />} />
-                        </Routes>
-                    </div>
-                </Layout>
-                <div className="footer">
-                    <Typography.Paragraph style={{ color: '#fff', textAlign: 'center' }}>
-                        Crypto Info <br />
-                        2022 React is fun :) <br />
-                        created by <a href="https://www.linkedin.com/in/albert-barsamov-2009041a0/" target="_blank" rel='noreferrer'>Albert</a>
-                    </Typography.Paragraph>
-                    <Space>
-                        <Link to='/'> Home</Link>
-                        <Link to='/exchanges'> Exchanges</Link>
-                        <Link to='/news'> News</Link>
-                    </Space>
-                </div>
-            </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="exchanges" element={<Exchanges />} />
+              <Route path="cryptos" element={<Cryptos />} />
+              <Route path="crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Paragraph style={{ color: "#fff", textAlign: "center" }}>
+            Crypto Info <br />
+            2022 React is fun :) <br />
+            created by{" "}
+            <a
+              href="https://www.linkedin.com/in/albert-64/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Albert
+            </a>
+          </Typography.Paragraph>
+          <Space>
+            <Link to="/"> Home</Link>
+            <Link to="/exchanges"> Exchanges</Link>
+            <Link to="/news"> News</Link>
+          </Space>
         </div>
+      </div>
+    </div>
+  );
+};
 
-
-
-    )
-}
-
-export default App
+export default App;
