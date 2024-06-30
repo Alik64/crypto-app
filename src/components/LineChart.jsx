6183b411f711a34cad4a4d0870d1140bb890f15a
Albert Barsamov <albert.barsamov@gmail.com>
2024-06-30 23:36:11 +0200

    update linechart fill + tension

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,46 +1,56 @@
-import React from 'react';
+import React from "react";
+import { Line } from "react-chartjs-2";
 
-import Chart from 'chart.js/auto'
-import { Line } from 'react-chartjs-2';
-
-import { Col, Row, Typography } from 'antd';
+import { Col, Row, Typography } from "antd";
 const { Title } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
-    const coinPrice = [];
-    const coinTimestamp = [];
+  const coinPrice = [];
+  const coinTimestamp = [];
 
-    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-        coinPrice.unshift(coinHistory?.data?.history[i].price);
-
-    }
-    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-        coinTimestamp.unshift(new Date(coinHistory?.data?.history[i].timestamp * 1000).toLocaleDateString());
-    }
-
-    const data = {
-        labels: coinTimestamp,
-        datasets: [{
-            label: 'Price In USD',
-            data: coinPrice,
-            fill: false,
-            backgroundColor: '#0071bd',
-            borderColor: '#0071bd',
-        }]
-    };
-
-    return (
-        <>
-            <Row className="chart-header">
-                <Title level={2} className="chart-title">{coinName} Price Chart </Title>
-                <Col className="price-container">
-                    <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
-                    <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
-                </Col>
-            </Row>
-            <Line data={data} />
-        </>
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.unshift(coinHistory?.data?.history[i].price);
+  }
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.unshift(
+      new Date(
+        coinHistory?.data?.history[i].timestamp * 1000
+      ).toLocaleDateString()
     );
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: "Price In USD",
+        data: coinPrice,
+        fill: true,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+        tension: 0.5,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">
+          {coinName} Price Chart{" "}
+        </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">
+            Change: {coinHistory?.data?.change}%
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: $ {currentPrice}
+          </Title>
+        </Col>
+      </Row>
+      <Line data={data} />
+    </>
+  );
 };
 
 export default LineChart;
